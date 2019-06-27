2b860f93ea841bd56cc68eea8747392623a5ae40
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-27 15:21:32 -0700

    exercise 2 echoing date with a second delay

--- client.js
+++ client.js
@@ -1,41 +1,57 @@
+// const { createConnection } = require('net');
+
+// const from = 'totesbeyonce@email.com';
+// const to = 'ellencrivella@gmail.com';
+
+// const email =  
+// `FROM: "Beyonce" <${from}>
+// To: "Ellen Crivella" <${to}>
+// Date: ${new Date()}
+// Subject: Whatever you want
+
+// Hoi hoi. Your FLS is learning how to take advantage of emails \r\n.\r\n
+// `;
+
+// //you must have the dns?
+// const mailServerAddress = 'gmail-smtp-in.l.google.com';
+
+// //number sotp server use
+// const client = createConnection(25, mailServerAddress, () => {
+//   console.log('connected');
+//   client.write('hello');
+// });
+
+// //HELO email is your domain name
+// const steps = [
+//   'HELO la.com\n',
+//   `MAIL FROM: <${from}>\n`,
+//   `RCPT TO: <${to}>\n`,
+//   'DATA\n',
+//   email,
+//   'QUIT\n'
+// ];
+
+// let step = 0;
+// client.on('data', data => {
+//   console.log(data.toString());
+
+//   if(step === steps.length) return client.end();
+//   client.write(steps[step]);
+//   step++;
+// });
+//.legnth of the buffer will return the legnth.
+
 const { createConnection } = require('net');
 
-const from = 'totesbeyonce@email.com';
-const to = 'ellencrivella@gmail.com';
+const client = createConnection(7891, () =>{
+  client.write('hello!');
 
-const email =  
-`FROM: "Beyonce" <${from}>
-To: "Ellen Crivella" <${to}>
-Date: ${new Date()}
-Subject: Whatever you want
-
-Hoi hoi. Your FLS is learning how to take advantage of emails \r\n.\r\n
-`;
-
-//you must have the dns?
-const mailServerAddress = 'gmail-smtp-in.l.google.com';
-
-//number sotp server use
-const client = createConnection(25, mailServerAddress, () => {
-  console.log('connected');
-  client.write('hello');
+  setInterval(() => {
+    client.write(new Date().toISOString());
+  }, 1000);
 });
 
-//HELO email is your domain name
-const steps = [
-  'HELO la.com\n',
-  `MAIL FROM: <${from}>\n`,
-  `RCPT TO: <${to}>\n`,
-  'DATA\n',
-  email,
-  'QUIT\n'
-];
-
-let step = 0;
-client.on('data', data => {
-  console.log(data.toString());
-
-  if(step === steps.length) return client.end();
-  client.write(steps[step]);
-  step++;
+client.on('data', echo => {
+  console.log(echo.toString());
 });
+
--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+// const { createServer } = require('net');
+
+// const server = createServer(sock => {
+//   console.log('client connected');
+//   sock.write('hello');
+
+//   sock.on('data', data => {
+//     console.log('from client', data.toString());
+//   });
+// });
+
+// server.listen(7891, () => {
+//   console.log('server is listening to port 7891')
+// });
+
 const { createServer } = require('net');
 
 const server = createServer(sock => {
-  console.log('client connected');
-  sock.write('hello');
+  sock.write('I will echo');
 
-  sock.on('data', data => {
-    console.log('from client', data.toString());
+  sock.on('data', echo => {
+    sock.write(echo);
   });
 });
 
-server.listen(7891, () => {
-  console.log('server is listening to port 7891')
-});
+server.listen(7891);
